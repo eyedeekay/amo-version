0ca9b2cf3f45f2a6d2bb10db48c51681358f4b9e
eyedeekay <idk@mulder>
2024-02-01 14:37:15 -0500

    undo umask thing

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"syscall"
 
 	amo "github.com/eyedeekay/amo-version/lib"
 )
@@ -98,10 +97,6 @@ func main() {
 		os.MkdirAll(*outdir, 0755)
 		amo.DownloadFile(*outpath)
 		if *extract {
-			oldmask := syscall.Umask(777)
-			if oldmask != 0 {
-				fmt.Println("UMASK set to 777 for process")
-			}
 			amo.Unzip(*outpath)
 		}
 	}
